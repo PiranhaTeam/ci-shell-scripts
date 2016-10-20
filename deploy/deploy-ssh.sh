23e397fe7dcf1f88836caf723b44bccc36599e6e
Bernardo Martínez Garrido <programming@wandrell.com>
2016-10-20 22:11:27 +0200

    Deployment configuration is now set through parameters

--- deploy/deploy-ssh.sh
+++ deploy/deploy-ssh.sh
@@ -5,21 +5,17 @@
 # This script will upload all the files in the current folder into a server
 # using a SSH connection.
 #
-# The connection configuration will be taken from the environmental variables.
-#
-# --- ENVIRONMENTAL VARIABLES ---
-#
-# The following environmental variables are required by the script:
-# - DEPLOY_USERNAME : string, username for the server
-# - DEPLOY_PASSWORD : string, password for the server
-# - DEPLOY_HOST : string, host for the server
-# - DEPLOY_PORT : string, port to use for the connection
-# - DEPLOY_PATH : string, path inside the server to deploy the files
+# The connection configuration will be set through parameters.
 #
 # -- PARAMETERS --
 #
 # The function expects the following parameters:
 # $1: A flow control boolean flag, indicating if the script should be run or not.
+# $2: username, for deployment server.
+# $3: password, for deployment server.
+# $4: host, for the server where the files will be deployed.
+# $5: port, for the server where the files will be deployed.
+# $6: path, location inside the server to deploy the files.
 #
 
 # Fails if any used variable is not set
@@ -28,13 +24,18 @@ set -o nounset
 set -e
 
 deploy=${1:-}
+username=${2:-}
+password=${3:-}
+host=${4:-}
+port=${5:-}
+path=${6:-}
 
 # Expects a flow control parameter
 if [ "${deploy}" == "true" ]; then
 
     echo "Deploying files"
 
-    sshpass -p "$DEPLOY_PASSWORD" scp -o 'StrictHostKeyChecking no' -P "$DEPLOY_PORT" -r ./* "$DEPLOY_USERNAME@$DEPLOY_HOST:$DEPLOY_PATH";
+    sshpass -p "$password" scp -o 'StrictHostKeyChecking no' -P "$port" -r ./* "$username@$host:$path";
 
 else
 
