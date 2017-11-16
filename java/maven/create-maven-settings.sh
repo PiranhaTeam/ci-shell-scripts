2e452b3fcf0bfe589c15b8741e09fa3e401cb17f
Bernardo <programming@wandrell.com>
2017-11-16 21:25:09 +0100

    Changed site URL variable name

--- java/maven/create-maven-settings.sh
+++ java/maven/create-maven-settings.sh
@@ -59,8 +59,8 @@
 # - DEPLOY_DOCS_DEVELOP_USER: string, user for the development documentation site repo
 # - DEPLOY_DOCS_DEVELOP_PASSWORD: string, password for the development documentation site repo
 #
-# - DEPLOY_SITE: string, path for deploying the Maven site
-# - DEPLOY_DEVELOP_SITE: string, path for deploying the development Maven site
+# - DEPLOY_DOCS_SITE: string, path for deploying the Maven site
+# - DEPLOY_DOCS_DEVELOP_SITE: string, path for deploying the development Maven site
 #
 
 # Fails if any used variable is not set
@@ -132,9 +132,9 @@ echo "Using release profile ${profile_release}";
          # This profile is used to define the deployment site URL
          echo "<properties>"
             # Release site
-            echo "<site.release.url>${DEPLOY_SITE}</site.release.url>"
+            echo "<site.release.url>${DEPLOY_DOCS_SITE}</site.release.url>"
             # Development site
-            echo "<site.develop.url>${DEPLOY_DEVELOP_SITE}</site.develop.url>"
+            echo "<site.develop.url>${DEPLOY_DOCS_DEVELOP_SITE}</site.develop.url>"
          echo "</properties>"
       echo "</profile>";
 
